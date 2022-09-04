096ee4d8acece0261484af7c70d8ce151416ad7a
worachatsun <worachat.sun@gmail.com>
2022-09-05 02:08:00 +0700

    setup font and MUI temporary theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { red } from '@mui/material/colors'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
 }
 
 export default MyApp
